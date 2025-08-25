0222f292ca27eac150c6158ae04ca22f54612a6a
Edson J. Vargas <edson.jhair@yahoo.com>
2025-08-25 17:04:02 -0500

    First complete revision

---
+++ main.js
@@ -0,0 +1,13 @@
+const closeMenu = document.getElementById("close-menu");
+const openMenu = document.getElementById("h-menu");
+const menu = document.getElementById("nav-menu");
+
+closeMenu.addEventListener("click", () => {
+  closeMenu.style.display = "none";
+  menu.classList.remove("tablet-mobile-open");
+});
+
+openMenu.addEventListener("click", () => {
+  closeMenu.style.display = "block";
+  menu.classList.add("tablet-mobile-open");
+});
