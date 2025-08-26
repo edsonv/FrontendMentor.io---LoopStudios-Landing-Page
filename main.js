2646094e53902792aacc80dc78ff1be1492a4504
Edson J. Vargas <edson.jhair@yahoo.com>
2025-08-26 13:06:47 -0500

    Updates

--- main.js
+++ main.js
@@ -2,12 +2,14 @@ const closeMenu = document.getElementById("close-menu");
 const openMenu = document.getElementById("h-menu");
 const menu = document.getElementById("nav-menu");
 
-closeMenu.addEventListener("click", () => {
-  closeMenu.style.display = "none";
-  menu.classList.remove("tablet-mobile-open");
-});
+if (openMenu && closeMenu && menu) {
+  closeMenu.addEventListener("click", () => {
+    closeMenu.style.display = "none";
+    menu.classList.remove("tablet-mobile-open");
+  });
 
-openMenu.addEventListener("click", () => {
-  closeMenu.style.display = "block";
-  menu.classList.add("tablet-mobile-open");
-});
+  openMenu.addEventListener("click", () => {
+    closeMenu.style.display = "block";
+    menu.classList.add("tablet-mobile-open");
+  });
+}
